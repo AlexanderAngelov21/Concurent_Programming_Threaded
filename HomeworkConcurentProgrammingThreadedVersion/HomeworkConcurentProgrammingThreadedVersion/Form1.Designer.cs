99ba55c825339157f0f05ec84c2836c0148d451e
Alexander Angelov <scorpiona_4500@abv.bg>
2022-12-02 19:39:25 +0200

    Uploading files

---
+++ HomeworkConcurentProgrammingThreadedVersion/HomeworkConcurentProgrammingThreadedVersion/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace HomeworkConcurentProgrammingThreadedVersion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancellationBtn = new System.Windows.Forms.Button();
+            this.countWordsBtn = new System.Windows.Forms.Button();
+            this.shortestWordBtn = new System.Windows.Forms.Button();
+            this.longestWordBtn = new System.Windows.Forms.Button();
+            this.averageCountBtn = new System.Windows.Forms.Button();
+            this.textBoxCountWords = new System.Windows.Forms.TextBox();
+            this.textBoxShortestWord = new System.Windows.Forms.TextBox();
+            this.textBoxLongestWord = new System.Windows.Forms.TextBox();
+            this.textBoxAverageLength = new System.Windows.Forms.TextBox();
+            this.mostCommonWordsBtm = new System.Windows.Forms.Button();
+            this.leastCommonWordsBtn = new System.Windows.Forms.Button();
+            this.textBox5MostCommon = new System.Windows.Forms.TextBox();
+            this.textBox5LeastCommon = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.percentageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cancellationBtn
+            // 
+            this.cancellationBtn.AutoSize = true;
+            this.cancellationBtn.Location = new System.Drawing.Point(639, 408);
+            this.cancellationBtn.Name = "cancellationBtn";
+            this.cancellationBtn.Size = new System.Drawing.Size(149, 30);
+            this.cancellationBtn.TabIndex = 0;
+            this.cancellationBtn.Text = "Cancellation button";
+            this.cancellationBtn.UseVisualStyleBackColor = true;
+            this.cancellationBtn.Click += new System.EventHandler(this.cancellationBtn_Click);
+            // 
+            // countWordsBtn
+            // 
+            this.countWordsBtn.AutoSize = true;
+            this.countWordsBtn.Location = new System.Drawing.Point(13, 12);
+            this.countWordsBtn.Name = "countWordsBtn";
+            this.countWordsBtn.Size = new System.Drawing.Size(197, 30);
+            this.countWordsBtn.TabIndex = 1;
+            this.countWordsBtn.Text = "Show me number of words";
+            this.countWordsBtn.UseVisualStyleBackColor = true;
+            this.countWordsBtn.Click += new System.EventHandler(this.countWordsBtn_Click);
+            // 
+            // shortestWordBtn
+            // 
+            this.shortestWordBtn.AutoSize = true;
+            this.shortestWordBtn.Location = new System.Drawing.Point(216, 12);
+            this.shortestWordBtn.Name = "shortestWordBtn";
+            this.shortestWordBtn.Size = new System.Drawing.Size(174, 30);
+            this.shortestWordBtn.TabIndex = 2;
+            this.shortestWordBtn.Text = "Show me shortest word";
+            this.shortestWordBtn.UseVisualStyleBackColor = true;
+            this.shortestWordBtn.Click += new System.EventHandler(this.shortestWordBtn_Click);
+            // 
+            // longestWordBtn
+            // 
+            this.longestWordBtn.AutoSize = true;
+            this.longestWordBtn.Location = new System.Drawing.Point(396, 12);
+            this.longestWordBtn.Name = "longestWordBtn";
+            this.longestWordBtn.Size = new System.Drawing.Size(171, 30);
+            this.longestWordBtn.TabIndex = 3;
+            this.longestWordBtn.Text = "Show me longest word";
+            this.longestWordBtn.UseVisualStyleBackColor = true;
+            this.longestWordBtn.Click += new System.EventHandler(this.longestWordBtn_Click);
+            // 
+            // averageCountBtn
+            // 
+            this.averageCountBtn.AutoSize = true;
+            this.averageCountBtn.Location = new System.Drawing.Point(573, 12);
+            this.averageCountBtn.Name = "averageCountBtn";
+            this.averageCountBtn.Size = new System.Drawing.Size(171, 30);
+            this.averageCountBtn.TabIndex = 4;
+            this.averageCountBtn.Text = "Average Word Length";
+            this.averageCountBtn.UseVisualStyleBackColor = true;
+            this.averageCountBtn.Click += new System.EventHandler(this.averageCountBtn_Click);
+            // 
+            // textBoxCountWords
+            // 
+            this.textBoxCountWords.Location = new System.Drawing.Point(13, 48);
+            this.textBoxCountWords.Multiline = true;
+            this.textBoxCountWords.Name = "textBoxCountWords";
+            this.textBoxCountWords.Size = new System.Drawing.Size(197, 124);
+            this.textBoxCountWords.TabIndex = 5;
+            // 
+            // textBoxShortestWord
+            // 
+            this.textBoxShortestWord.Location = new System.Drawing.Point(216, 48);
+            this.textBoxShortestWord.Multiline = true;
+            this.textBoxShortestWord.Name = "textBoxShortestWord";
+            this.textBoxShortestWord.Size = new System.Drawing.Size(174, 124);
+            this.textBoxShortestWord.TabIndex = 6;
+            // 
+            // textBoxLongestWord
+            // 
+            this.textBoxLongestWord.Location = new System.Drawing.Point(396, 48);
+            this.textBoxLongestWord.Multiline = true;
+            this.textBoxLongestWord.Name = "textBoxLongestWord";
+            this.textBoxLongestWord.Size = new System.Drawing.Size(171, 124);
+            this.textBoxLongestWord.TabIndex = 7;
+            // 
+            // textBoxAverageLength
+            // 
+            this.textBoxAverageLength.Location = new System.Drawing.Point(573, 48);
+            this.textBoxAverageLength.Multiline = true;
+            this.textBoxAverageLength.Name = "textBoxAverageLength";
+            this.textBoxAverageLength.Size = new System.Drawing.Size(171, 124);
+            this.textBoxAverageLength.TabIndex = 8;
+            // 
+            // mostCommonWordsBtm
+            // 
+            this.mostCommonWordsBtm.AutoSize = true;
+            this.mostCommonWordsBtm.Location = new System.Drawing.Point(13, 178);
+            this.mostCommonWordsBtm.Name = "mostCommonWordsBtm";
+            this.mostCommonWordsBtm.Size = new System.Drawing.Size(206, 30);
+            this.mostCommonWordsBtm.TabIndex = 9;
+            this.mostCommonWordsBtm.Text = "5 Most common words";
+            this.mostCommonWordsBtm.UseVisualStyleBackColor = true;
+            this.mostCommonWordsBtm.Click += new System.EventHandler(this.mostCommonWordsBtm_Click);
+            // 
+            // leastCommonWordsBtn
+            // 
+            this.leastCommonWordsBtn.AutoSize = true;
+            this.leastCommonWordsBtn.Location = new System.Drawing.Point(225, 178);
+            this.leastCommonWordsBtn.Name = "leastCommonWordsBtn";
+            this.leastCommonWordsBtn.Size = new System.Drawing.Size(203, 30);
+            this.leastCommonWordsBtn.TabIndex = 10;
+            this.leastCommonWordsBtn.Text = "5 Least common words";
+            this.leastCommonWordsBtn.UseVisualStyleBackColor = true;
+            this.leastCommonWordsBtn.Click += new System.EventHandler(this.leastCommonWordsBtn_Click);
+            // 
+            // textBox5MostCommon
+            // 
+            this.textBox5MostCommon.Location = new System.Drawing.Point(12, 214);
+            this.textBox5MostCommon.Multiline = true;
+            this.textBox5MostCommon.Name = "textBox5MostCommon";
+            this.textBox5MostCommon.Size = new System.Drawing.Size(207, 196);
+            this.textBox5MostCommon.TabIndex = 11;
+            // 
+            // textBox5LeastCommon
+            // 
+            this.textBox5LeastCommon.Location = new System.Drawing.Point(225, 214);
+            this.textBox5LeastCommon.Multiline = true;
+            this.textBox5LeastCommon.Name = "textBox5LeastCommon";
+            this.textBox5LeastCommon.Size = new System.Drawing.Size(203, 196);
+            this.textBox5LeastCommon.TabIndex = 12;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(486, 408);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(125, 29);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // percentageLabel
+            // 
+            this.percentageLabel.AutoSize = true;
+            this.percentageLabel.Location = new System.Drawing.Point(522, 379);
+            this.percentageLabel.Name = "percentageLabel";
+            this.percentageLabel.Size = new System.Drawing.Size(0, 20);
+            this.percentageLabel.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.percentageLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox5LeastCommon);
+            this.Controls.Add(this.textBox5MostCommon);
+            this.Controls.Add(this.leastCommonWordsBtn);
+            this.Controls.Add(this.mostCommonWordsBtm);
+            this.Controls.Add(this.textBoxAverageLength);
+            this.Controls.Add(this.textBoxLongestWord);
+            this.Controls.Add(this.textBoxShortestWord);
+            this.Controls.Add(this.textBoxCountWords);
+            this.Controls.Add(this.averageCountBtn);
+            this.Controls.Add(this.longestWordBtn);
+            this.Controls.Add(this.shortestWordBtn);
+            this.Controls.Add(this.countWordsBtn);
+            this.Controls.Add(this.cancellationBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button cancellationBtn;
+        private Button countWordsBtn;
+        private Button shortestWordBtn;
+        private Button longestWordBtn;
+        private Button averageCountBtn;
+        private TextBox textBoxCountWords;
+        private TextBox textBoxShortestWord;
+        private TextBox textBoxLongestWord;
+        private TextBox textBoxAverageLength;
+        private Button mostCommonWordsBtm;
+        private Button leastCommonWordsBtn;
+        private TextBox textBox5MostCommon;
+        private TextBox textBox5LeastCommon;
+        private ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Label percentageLabel;
+    }
+}
---
+++ HomeworkConcurentProgrammingThreadedVersion/HomeworkConcurentProgrammingThreadedVersion/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HomeworkConcurentProgrammingThreadedVersion
+{
+    public partial class Form1 : Form
+    {
+        static string filePath = "C:\\Users\\Asi\\Desktop\\book.txt";
+        static FileStream fs = new FileStream(filePath, FileMode.Open);
+        static StreamReader sr = new StreamReader(fs);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        CancellationTokenSource cts;
+        private void cancellationBtn_Click(object sender, EventArgs e)
+        {
+            if (cts != null) cts.Cancel();
+            backgroundWorker1.CancelAsync();
+        }
+        delegate void SetTextCallback(string text);
+        private void SetTextForCountTestBox(string text)
+        {
+            
+            if (this.textBoxCountWords.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetTextForCountTestBox);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBoxCountWords.Text = text;
+            }
+        }
+        private void SetTextForShortestTextBox(string text)
+        {
+
+            if (this.textBoxShortestWord.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetTextForShortestTextBox);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBoxShortestWord.Text = text;
+            }
+        }
+        private void SetTextForLongestTextBox(string text)
+        {
+
+            if (this.textBoxLongestWord.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetTextForLongestTextBox);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBoxLongestWord.Text = text;
+            }
+        }
+        private void SetTextForAverageLengthTextBox(string text)
+        {
+
+            if (this.textBoxAverageLength.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetTextForAverageLengthTextBox);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBoxAverageLength.Text = text;
+            }
+        }
+        private void SetTextForMostCommonTextBox(string text)
+        {
+
+            if (this.textBox5MostCommon.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetTextForMostCommonTextBox);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox5MostCommon.Text += text;
+            }
+        }
+        private void SetTextForLeastCommonTextBox(string text)
+        {
+
+            if (this.textBox5LeastCommon.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetTextForLeastCommonTextBox);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox5LeastCommon.Text += text;
+            }
+        }
+     
+        private void WordCountWorker(object tkn)
+        {
+            CancellationToken token = (CancellationToken)tkn;
+            string content = sr.ReadToEnd();
+            content = content.Replace("\r\n", "\r");
+            int charCount = content.Length;
+            int lineCount = content.Split('\r').Length;
+            content = content.Replace('\r', ' ');
+            List<string> words = content.Split(' ').ToList();
+            List<string> countWords = new List<string>();
+            foreach (string word in words)
+            {
+                countWords.Add(Regex.Match(word, @"\w{3,}").Value);
+                if (token.IsCancellationRequested) break;
+            }
+            
+            int WordsCount = countWords.Count;
+            SetTextForCountTestBox($"Брой думи с 3 или повече букви: {WordsCount} ,общ брой букви на думите: {charCount} ,редове: {lineCount}");
+            BeginInvoke((Action)(() => countWordsBtn.Enabled = true));
+          
+        }
+        private void ShortestWordWorker(object tkn)
+        {
+            CancellationToken token = (CancellationToken)tkn;
+            string content = sr.ReadToEnd();
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '!', '?' };
+            List<string> list = content.Split(delimiterChars).ToList();
+            string shortestWord = list.OrderBy(n => n.Length).Where(n => n.Length >= 3).FirstOrDefault();
+            SetTextForShortestTextBox(shortestWord);
+            BeginInvoke((Action)(() => shortestWordBtn.Enabled = true));
+        }
+        private void LongestWordWorker(object tkn)
+        {
+            CancellationToken token = (CancellationToken)tkn;
+            string content = sr.ReadToEnd();
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '!', '?' };
+            List<string> list = content.Split(delimiterChars).ToList();
+            string longestWord = list.OrderByDescending(n => n.Length).Where(n => n.Length >= 3).FirstOrDefault();
+            SetTextForLongestTextBox(longestWord);
+            BeginInvoke((Action)(() => longestWordBtn.Enabled = true));
+        }
+        private void AverageLengthWorker(object tkn)
+        {
+            string content = sr.ReadToEnd();
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '!', '?' };
+            var list = content.Split(delimiterChars).ToList();
+            var averageCount = list.Average(n => n.Length);
+            SetTextForAverageLengthTextBox($"Средна дължина на думите(букви): {averageCount.ToString("#.##")}");
+
+            BeginInvoke((Action)(() => averageCountBtn.Enabled = true));
+        }
+        private void FiveMostCommonWorker(object tkn)
+        {
+            string content = sr.ReadToEnd();
+            string[] splitWords = content.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var commonWords = splitWords.ToList().GroupBy(e => e).Select(g => new { Value = g.Key, Count = g.Count() }).OrderByDescending(e => e.Count).Where(l => l.Value.Length >= 3).Take(5);
+            foreach (var x in commonWords)
+            {
+                var temp = x.Value;
+                var temp2 = x.Count;
+                SetTextForMostCommonTextBox($"Дума: \"{temp}\" среща се - \"{temp2}\" пъти. ");  
+            }
+   
+
+            BeginInvoke((Action)(() => mostCommonWordsBtm.Enabled = true));
+        }
+        private void FiveLeastCommonWorker(object tkn)
+        {
+            string content = sr.ReadToEnd();
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '!', '?' };
+
+            string[] splitWords = content.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            var commonWords = splitWords.ToList().GroupBy(e => e).Select(g => new { Value = g.Key, Count = g.Count() }).OrderBy(e => e.Count).Where(l => l.Value.Length >= 3).Take(5);
+            foreach (var x in commonWords)
+            {
+                SetTextForLeastCommonTextBox($"Дума: \"{x.Value}\" среща се - \"{x.Count}\" пъти. ");
+            }
+            BeginInvoke((Action)(() => leastCommonWordsBtn.Enabled = true));
+        }
+        private void countWordsBtn_Click(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            var thread = new Thread(WordCountWorker);
+            thread.IsBackground = true;
+            thread.Start(cts.Token);
+            countWordsBtn.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private void shortestWordBtn_Click(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            var thread = new Thread(ShortestWordWorker);
+            thread.IsBackground = true;
+            thread.Start(cts.Token);
+            shortestWordBtn.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private void longestWordBtn_Click(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            var thread = new Thread(LongestWordWorker);
+            thread.IsBackground = true;
+            thread.Start(cts.Token);
+            longestWordBtn.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private void averageCountBtn_Click(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            var thread = new Thread(AverageLengthWorker);
+            thread.IsBackground = true;
+            thread.Start(cts.Token);
+            averageCountBtn.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+            
+        }
+
+        private void mostCommonWordsBtm_Click(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            var thread = new Thread(FiveMostCommonWorker);
+            thread.IsBackground = true;
+            thread.Start(cts.Token);
+            mostCommonWordsBtm.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private void leastCommonWordsBtn_Click(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            var thread = new Thread(FiveLeastCommonWorker);
+            thread.IsBackground = true;
+            thread.Start(cts.Token);
+            leastCommonWordsBtn.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    simulateHeavyJob();
+                    backgroundWorker1.ReportProgress(i);
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            percentageLabel.Text = e.ProgressPercentage.ToString() + " %";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                display("You have cancelled");
+                progressBar1.Value = 0;
+                percentageLabel.Text = "0";
+            }
+            else
+            {
+                display("Work completed succesfully");
+            }
+        }
+        private void simulateHeavyJob()
+        {
+            Thread.Sleep(100);
+        }
+        private void display(string text)
+        {
+            MessageBox.Show(text);
+        }
+    }
+}
